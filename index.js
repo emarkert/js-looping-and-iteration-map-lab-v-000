091cd9d48e462a4ab7923abb72e7eda7f01bf375
Evan  Markert <evanmarkert@gmail.com>
2019-08-04 16:15:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function lowerCaseDrivers(arr) {
 //accept array, split names by space, assign names to a collection with firstName and lastName as keys as their values
 function nameToAttributes(arr) {
   let newArr = arr.map(name => (name.split(" "){firstName: name[0], lastName[1]}));
+  return newArr;
 }
 
 function attributesToPhrase() {
