60ce6c58450d6769f14f7f930a2a06d7cab45b18
Evan  Markert <evanmarkert@gmail.com>
2019-08-04 16:44:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,27 +6,17 @@ function lowerCaseDrivers(arr) {
     return newArr;
 }
 
-// //accept array, split names by space, assign names to a collection with firstName and lastName as keys as their values
-// function nameToAttributes(arr) {
-//     return arr.map(function(e) {
-//       let name = e.split(" ")
-//         return {firstName: name[0], lastName: name[1]}
-//       })
-// }
-
-// //accept an array with an object, return a string for each pair saying "[name] is from [hometowm]"
-// function attributesToPhrase(arr) {
-//   return arr.map(function(e) {
-//     return `${e.name} is from ${e.hometown}`
-//   } )
-// }
-
-
 //accept array, split names by space, assign names to a collection with firstName and lastName as keys as their values
 function nameToAttributes(arr) {
-    let newArr =  arr.map(e => {
+    return arr.map(function(e) {
       let name = e.split(" ")
         return {firstName: name[0], lastName: name[1]}
-          return newArr;
       })
+}
+
+//accept an array with an object, return a string for each pair saying "[name] is from [hometowm]"
+function attributesToPhrase(arr) {
+  return arr.map(function(e) {
+    return `${e.name} is from ${e.hometown}`
+  } )
 }
